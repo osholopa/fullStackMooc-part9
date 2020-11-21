71e1807405a67a021a31f184124473d67eda13ef
Oskari Holopainen <oskariholopa@gmail.com>
2020-11-21 13:22:30 +0200

    Exercise 9.17: patientor, step2

---
+++ patientor/src/PatientDetailsPage/index.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import axios from "axios";
+import { Container, Header, Icon } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+import { Patient } from "../types";
+
+const PatientDetailsPage: React.FC = () => {
+  const id = useParams<{ id: string }>().id;
+  const [{ patients }, dispatch] = useStateValue();
+
+  const isEmpty = (obj: Record<string, Patient>): boolean => {
+    return !Object.keys(obj).length;
+  };
+
+  React.useEffect(() => {
+    const fetchPatientDetails = async (patient: Patient) => {
+      try {
+        if (!patient.ssn || !patient.entries) {
+          const { data: patientDetails } = await axios.get(
+            `${apiBaseUrl}/patients/${id}`
+          );
+          dispatch({ type: "UPDATE_PATIENT", payload: patientDetails });
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    if (!isEmpty(patients)) {
+      fetchPatientDetails(patients[id]);
+    }
+  }, [patients, id, dispatch]);
+
+  return (
+    <div>
+      <Container textAlign="left">
+        <Header as="h1">Patient details</Header>
+
+        {!isEmpty(patients) ? (
+          <>
+            <Header as="h2">
+              {patients[id].name}{" "}
+              {patients[id].gender === "female" ? (
+                <Icon name="venus" />
+              ) : (
+                <Icon name="mars" />
+              )}
+            </Header>
+            <p>date of birth: {patients[id].dateOfBirth}</p>
+            <p>ssn: {patients[id].ssn}</p>
+            <p>occupation: {patients[id].occupation}</p>
+          </>
+        ) : null}
+      </Container>
+    </div>
+  );
+};
+
+export default PatientDetailsPage;
